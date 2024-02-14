3cb0c792b7641162fcec27ce4cf2196441e83f68
Arsen Hryhoriak <arsen.hryh@gmail.com>
2024-02-14 14:30:59 +0200

    app & images & icons

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,18 @@ import CardList from "./components/CardList";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Social from "./components/Social";
+import { useMediaQuery } from "react-responsive";
+import "swiper/css/bundle";
 
 function App() {
+  const isDesktop = useMediaQuery({ query: "(min-width: 1280px)" });
   return (
     <>
-      <Header />
+      <Header isDesktop={isDesktop} />
       <main>
         <Hero />
-        <About />
-        <CardList />
+        <About isDesktop={isDesktop} />
+        <CardList isDesktop={isDesktop} />
         <Contact />
         <Social />
       </main>
